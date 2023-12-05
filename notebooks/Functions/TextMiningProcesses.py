1c42dd9670a5cc4751f8d736e72f3edc96308643
Adam Cleaver <69239226+Scratticus@users.noreply.github.com>
2023-12-05 19:50:39 +0100

    Updates for the report

--- notebooks/Functions/TextMiningProcesses.py
+++ notebooks/Functions/TextMiningProcesses.py
@@ -1,15 +1,3 @@
-# Function to perform lemmatization and remove stopwords
-def remove_text_between_tags(text):
-    import re
-    if isinstance(text, list):
-        # If 'reviewText' is a list, apply the function to each element of the list
-        return [re.sub(r'<.*?>.*?<.*?>', '', item) if isinstance(item, str) else item for item in text]
-    elif isinstance(text, str):
-        # If 'reviewText' is a string, apply the function directly
-        return re.sub(r'<.*?>.*?<.*?>', '', text)
-    else:
-        return text
-
 def column_lemmatizer(text_series):
     """
     This function preprocesses a pandas Series of sentences, typically taken from a dataframe column and prepares them for classification/regression modelling
@@ -33,6 +21,12 @@ def column_lemmatizer(text_series):
     from nltk.corpus import stopwords
     from nltk.stem import WordNetLemmatizer
     from nltk.tokenize.regexp import RegexpTokenizer
+    from spellchecker import SpellChecker
+    from tqdm import tqdm
+    import re
+
+    tokenizer = RegexpTokenizer(r'[a-zA-Z]+')
+    spell = SpellChecker()
 
     # Download NLTK resources
     if not nltk.corpus.stopwords.fileids():
@@ -43,35 +37,29 @@ def column_lemmatizer(text_series):
     # Initialize the lemmatizer and stopwords set
     lemmatizer = WordNetLemmatizer()
     stop_words = set(stopwords.words('english'))
-    tokenizer = RegexpTokenizer(r'[a-zA-Z]+')
 
-    lemmed_cells = []
+    if isinstance(text_series, pd.Series):
+        # If 'reviewText' is a list, apply the function to each element of the list
+        lemmed_cells =[]
+        with tqdm(total=len(text_series)) as pbar1:
+            for item in text_series:
+                if isinstance(item, str):
+                    item_no_tags = re.sub(r'<.*?>.*?<.*?>', '', item)
+                    item_tokens = tokenizer.tokenize(item_no_tags.lower())
+                    token_list = []
+                    for token in item_tokens:
+                        corrected_token = spell.correction(token.strip())
+                        if corrected_token and corrected_token not in stop_words:
+                            lemmed_word = lemmatizer.lemmatize(corrected_token)
+                            token_list.append(lemmed_word)     
+                    lemmed_cells.append(token_list)
+                    pbar1.update(1)
 
-    text_series = remove_text_between_tags(text_series)
+            lemmed_series = pd.Series(lemmed_cells)
 
-    for i in text_series:
-        
-        #tokenize series to lower case
-        tokens = tokenizer.tokenize(i.lower())
-
-        # remove stop words
-        stopped = []
-        for token in tokens:
-            if token not in stop_words:
-                stopped.append(token)
-
-        lemmed = ''
-        for word in stopped:
-            lemmed_word = lemmatizer.lemmatize(word)
-            lemmed += lemmed_word + ' '
-
-        lemmed.strip()
-
-        lemmed_cells.append(lemmed)
-
-    lemmed_series = pd.Series(lemmed_cells)
-
-    return lemmed_series
+            return lemmed_series
+    else:
+        raise TypeError('function must take a pd.Series as argument')
 
 def column_stemmatizer(text_series):
     """
@@ -96,6 +84,12 @@ def column_stemmatizer(text_series):
     from nltk.corpus import stopwords
     from nltk.stem.snowball import EnglishStemmer
     from nltk.tokenize.regexp import RegexpTokenizer
+    from spellchecker import SpellChecker
+    from tqdm import tqdm
+    import re
+
+    tokenizer = RegexpTokenizer(r'[a-zA-Z]+')
+    spell = SpellChecker()
 
     # Download NLTK resources
     if not nltk.corpus.stopwords.fileids():
@@ -105,37 +99,29 @@ def column_stemmatizer(text_series):
     # Initialize stemmer, stopwords and regex tokenizer
     stemmer = EnglishStemmer()
     stop_words = set(stopwords.words('english'))
-    tokenizer = RegexpTokenizer(r'[a-zA-Z]+')
 
-    # prepare list to hold returned data
-    stemmed_cells = []
+    if isinstance(text_series, pd.Series):
+        # If 'reviewText' is a list, apply the function to each element of the list
+        stemmed_cells =[]
+        with tqdm(total=len(text_series)) as pbar1:
+            for item in text_series:
+                if isinstance(item, str):
+                    item_no_tags = re.sub(r'<.*?>.*?<.*?>', '', item)
+                    item_tokens = tokenizer.tokenize(item_no_tags.lower())
+                    token_list = []
+                    for token in item_tokens:
+                        corrected_token = spell.correction(token)
+                        if corrected_token and corrected_token not in stop_words:
+                            stemmed_word = stemmer.stem(corrected_token)
+                            token_list.append(stemmed_word) 
+                    stemmed_cells.append(token_list)
+                    pbar1.update(1)
 
-    text_series = remove_text_between_tags(text_series)
+            stemmed_series = pd.Series(stemmed_cells)
 
-    # Loop through columns:
-    for i in text_series:
-
-        # Tokenize series and set lower case
-        tokens = tokenizer.tokenize(i.lower())
-
-        # remove stopwords
-        stopped = []
-        for token in tokens:
-            if token not in stop_words:
-                stopped.append(token)
-        
-        stemmed = ''
-        for word in stopped:
-            stemmed_word = stemmer.stem(word)
-            stemmed += stemmed_word + ' '
-        
-        stemmed.strip()
-
-        stemmed_cells.append(stemmed)
-
-    stemmed_series = pd.Series(stemmed_cells)
-
-    return stemmed_series
+            return stemmed_series
+    else:
+        raise TypeError('function must take a pd.Series as argument')
 
 def count_vectorize_data(X_train_processed, X_test_processed=None, max_features=None):
     """
